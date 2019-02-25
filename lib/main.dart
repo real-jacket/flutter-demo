78f372a9783e86db89557f38a1c6080475c3a4e8
kete <1762982273@qq.com>
2019-02-25 18:01:31 +0800

    formate code

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,8 @@ class MyApp extends StatelessWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _saved =new Set<WordPair>();
-  final _biggerFont  =const TextStyle(fontSize: 18.0);
+  final _saved = new Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
   Widget build(BuildContext context) {
@@ -41,31 +41,31 @@ class RandomWordsState extends State<RandomWords> {
     // return Text(wordPair.asPascalCase);
     return new Scaffold(
       appBar: new AppBar(
-      title: new Text('Startup Name Generator'),
-      actions: <Widget>[
-        new IconButton(icon: new Icon(Icons.list),onPressed:_pushSaved)
-      ],
+        title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
 
-  Widget _buildSuggestions(){
+  Widget _buildSuggestions() {
     return new ListView.builder(
       padding: const EdgeInsets.all(16.0),
       // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
       // 在偶数行，该函数会为单词对添加一个ListTile row.
       // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
       // 注意，在小屏幕上，分割线看起来可能比较吃力。
-      itemBuilder: (context,i){
+      itemBuilder: (context, i) {
         // 在每一列之前，添加一个1像素高的分隔线widget
         if (i.isOdd) return new Divider();
 
         // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
         // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
-        final index = i ~/2;
+        final index = i ~/ 2;
         // 如果是建议列表中最后一个单词对
-        if(index >=_suggestions.length){
+        if (index >= _suggestions.length) {
           // ...接着再生成10个单词对，然后添加到建议列表
           _suggestions.addAll(generateWordPairs().take(10));
         }
@@ -74,59 +74,55 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
-  Widget _buildRow(WordPair pair){
+  Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
-        style:_biggerFont,
+        style: _biggerFont,
       ),
-      trailing: new Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color:alreadySaved ? Colors.red : null
-      ),
-      onTap: (){
+      trailing: new Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null),
+      onTap: () {
         setState(() {
-         if (alreadySaved) {
-           _saved.remove(pair);
-         }else{
-           _saved.add(pair);
-         }
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
         });
       },
     );
   }
 
-  void _pushSaved(){
-       Navigator.of(context).push(
-         new MaterialPageRoute(
-           builder: (context){
-             final tiles =_saved.map(
-                (pair){
-                  return new ListTile(
-                    title: new Text(
-                      pair.asPascalCase,
-                      style:_biggerFont,
-                    ),
-                  );
-                },
-              ); 
-              final divided =ListTile
-                .divideTiles(
-                  context: context,
-                  tiles: tiles,
-                )
-                .toList();
-              
-              return new Scaffold(
-                appBar: new AppBar(
-                  title: new Text('Saved Suggestions'),
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
                 ),
-                body: new ListView(children: divided),
               );
-           },
-         ),
-       );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Saved Suggestions'),
+            ),
+            body: new ListView(children: divided),
+          );
+        },
+      ),
+    );
   }
 }
 
@@ -134,5 +130,3 @@ class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => new RandomWordsState();
 }
-
-
