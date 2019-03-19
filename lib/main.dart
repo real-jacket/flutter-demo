91d7ef93171fb442c4aa556146efcbdad99c4bfc
kete <1762982273@qq.com>
2019-03-19 11:44:58 +0800

    CustomScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
@@ -11,55 +10,64 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primaryColor: Colors.blue,
       ),
-      home: Homepage(),
+      home: CustomScrollViewTestRoute(),
     );
   }
 }
 
-class Homepage extends StatelessWidget {
+class CustomScrollViewTestRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('无限上拉列表'),
+    return Material(
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: 250,
+            flexibleSpace: FlexibleSpaceBar(
+              title: const Text('demo'),
+              background: Image.network(
+                'http://easyread.ph.126.net/pg-5nIfYApOfzPab3hWobA==/7806611722048198046.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(8.0),
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 10.0,
+                crossAxisSpacing: 10.0,
+                childAspectRatio: 4.0,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Container(
+                    alignment: Alignment.center,
+                    color: Colors.cyan[100 * (index % 9)],
+                    child: Text('grid item $index'),
+                  );
+                },
+                childCount: 20,
+              ),
+            ),
+          ),
+          SliverFixedExtentList(
+            itemExtent: 50.0,
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.lightBlue[100 * (index % 9)],
+                  child: Text('list item $index'),
+                );
+              },
+              childCount: 50,
+            ),
+          )
+        ],
       ),
-      body: InfiniteListView(),
-    );
-  }
-}
-
-class InfiniteListView extends StatefulWidget {
-  @override
-  _InfiniteListViewState createState() => _InfiniteListViewState();
-}
-
-class _InfiniteListViewState extends State<InfiniteListView> {
-  static const loadingTag = '##loading##';
-  var _words = <String>[loadingTag];
-
-  @override
-  void initState() {}
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ListTile(
-          title: Text(
-            '商品列表',
-            style: TextStyle(color: Colors.red),
-          ),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                title: Text('${index + 1}'),
-              );
-            },
-          ),
-        )
-      ],
     );
   }
 }
