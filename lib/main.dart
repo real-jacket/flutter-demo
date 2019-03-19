c9168f20cc424eb853caf7b86c68e2f89bb98324
kete <1762982273@qq.com>
2019-03-19 14:29:22 +0800

    滚动监听 NotificationListener

--- lib/main.dart
+++ lib/main.dart
@@ -10,70 +10,58 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primaryColor: Colors.blue,
       ),
-      home: ScrollControllerTestRoute(),
+      home: ScrollNotificationTestRoute(),
     );
   }
 }
 
-class ScrollControllerTestRoute extends StatefulWidget {
+class ScrollNotificationTestRoute extends StatefulWidget {
   @override
-  _ScrollControllerTestRouteState createState() =>
-      _ScrollControllerTestRouteState();
+  _ScrollNotificationTestRouteState createState() =>
+      _ScrollNotificationTestRouteState();
 }
 
-class _ScrollControllerTestRouteState extends State<ScrollControllerTestRoute> {
-  ScrollController _controller = ScrollController();
-  bool showToButton = false;
-
-  @override
-  void initState() {
-    _controller.addListener(() {
-      print(_controller.position);
-      if (_controller.offset < 1000 && showToButton) {
-        setState(() {
-          showToButton = false;
-        });
-      } else if (_controller.offset >= 1000 && showToButton == false) {
-        setState(() {
-          showToButton = true;
-        });
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+class _ScrollNotificationTestRouteState
+    extends State<ScrollNotificationTestRoute> {
+  String _progress = '0%';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('滚动控制'),
+        title: Text('滚动监听'),
       ),
       body: Scrollbar(
-        child: ListView.builder(
-          itemCount: 100,
-          itemExtent: 50.0,
-          controller: _controller,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('$index'),
-            );
+        child: NotificationListener<ScrollNotification>(
+          onNotification: (ScrollNotification notification) {
+            double progress = notification.metrics.pixels /
+                notification.metrics.maxScrollExtent;
+            setState(() {
+              _progress = "${(progress * 100).toInt()}%";
+            });
+            print("Bottom:${notification.metrics.extentAfter == 0}");
           },
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              ListView.builder(
+                itemCount: 100,
+                itemExtent: 50,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('$index'),
+                  );
+                },
+              ),
+              CircleAvatar(
+                radius: 30,
+                child: Text(_progress),
+                backgroundColor: Colors.black54,
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: !showToButton
-          ? null
-          : FloatingActionButton(
-              child: Icon(Icons.arrow_upward),
-              onPressed: () {
-                _controller.animateTo(.0,
-                    duration: Duration(microseconds: 200), curve: Curves.ease);
-              },
-            ),
     );
   }
 }
