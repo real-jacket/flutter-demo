fa3926f5a6cca51eba1cce4d57d5f25e880cbb82
kete <1762982273@qq.com>
2019-03-19 11:02:37 +0800

    无限加载列表

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
@@ -15,35 +16,74 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Homepage extends StatelessWidget{
+class Homepage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Demo'),
+        title: Text('无限上拉列表'),
       ),
-      body: SingleChildScrollViewTestRoute(),
+      body: InfiniteListView(),
     );
   }
 }
 
-class SingleChildScrollViewTestRoute extends StatelessWidget {
+class InfiniteListView extends StatefulWidget {
+  @override
+  _InfiniteListViewState createState() => _InfiniteListViewState();
+}
+
+class _InfiniteListViewState extends State<InfiniteListView> {
+  static const loadingTag = '##loading##';
+  var _words = <String>[loadingTag];
+
+  @override
+  void initState() {
+    _retrieveDate();
+  }
+
   @override
   Widget build(BuildContext context) {
-    String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    return Scrollbar(
-      child: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Center(
-          child: Column( 
-            //动态创建一个List<Widget>  
-            children: str.split("") 
-                //每一个字母都用一个Text显示,字体为原来的两倍
-                .map((c) => Text(c, textScaleFactor: 2.0,)) 
-                .toList(),
+    return ListView.separated(
+      itemCount: _words.length,
+      itemBuilder: (context, index) {
+        if (_words[index] == loadingTag) {
+          if (_words.length - 1 < 100) {
+            _retrieveDate();
+            return Container(
+              padding: const EdgeInsets.all(16.0),
+              alignment: Alignment.center,
+              child: SizedBox(
+                width: 24.0,
+                height: 24.0,
+                child: CircularProgressIndicator(
+                  strokeWidth: 2.0,
+                ),
+              ),
+            );
+          } else {
+            return Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(16.0),
+              child: Text('没有更多了', style: TextStyle(color: Colors.grey)),
+            );
+          }
+        }
+        return ListTile(
+          title: Text(_words[index]),
+        );
+      },
+      separatorBuilder: (context, index) => Divider(
+            height: .0,
           ),
-        ),
-      ),
     );
+  }
+
+  void _retrieveDate() {
+    Future.delayed(Duration(seconds: 2)).then((e) {
+      _words.insertAll(_words.length - 1,
+          generateWordPairs().take(20).map((e) => e.asPascalCase).toList());
+      setState(() {});
+    });
   }
 }
