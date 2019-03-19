f20564365a2cacbc9ac3e252280747ed41063e7b
kete <1762982273@qq.com>
2019-03-19 11:15:52 +0800

    固定表头

--- lib/main.dart
+++ lib/main.dart
@@ -38,52 +38,28 @@ class _InfiniteListViewState extends State<InfiniteListView> {
   var _words = <String>[loadingTag];
 
   @override
-  void initState() {
-    _retrieveDate();
-  }
+  void initState() {}
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: _words.length,
-      itemBuilder: (context, index) {
-        if (_words[index] == loadingTag) {
-          if (_words.length - 1 < 100) {
-            _retrieveDate();
-            return Container(
-              padding: const EdgeInsets.all(16.0),
-              alignment: Alignment.center,
-              child: SizedBox(
-                width: 24.0,
-                height: 24.0,
-                child: CircularProgressIndicator(
-                  strokeWidth: 2.0,
-                ),
-              ),
-            );
-          } else {
-            return Container(
-              alignment: Alignment.center,
-              padding: EdgeInsets.all(16.0),
-              child: Text('没有更多了', style: TextStyle(color: Colors.grey)),
-            );
-          }
-        }
-        return ListTile(
-          title: Text(_words[index]),
-        );
-      },
-      separatorBuilder: (context, index) => Divider(
-            height: .0,
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: Text(
+            '商品列表',
+            style: TextStyle(color: Colors.red),
           ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                title: Text('${index + 1}'),
+              );
+            },
+          ),
+        )
+      ],
     );
-  }
-
-  void _retrieveDate() {
-    Future.delayed(Duration(seconds: 2)).then((e) {
-      _words.insertAll(_words.length - 1,
-          generateWordPairs().take(20).map((e) => e.asPascalCase).toList());
-      setState(() {});
-    });
   }
 }
