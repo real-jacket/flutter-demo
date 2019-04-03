5651d5aec0460c854386fa1c488d87e344d1d860
kete <1762982273@qq.com>
2019-04-03 16:38:45 +0800

    数据共享-inheritedwidget

--- lib/main.dart
+++ lib/main.dart
@@ -28,33 +28,78 @@ class FlexLayoutTestRoute extends StatelessWidget {
       appBar: new AppBar(
         title: new Text("Circular Percent Indicators"),
       ),
-      body: Center(child: WillPopScopeTestRoute()),
+      body: Center(child: InheritedWidgetTestRoute()),
     );
   }
 }
 
-class WillPopScopeTestRoute extends StatefulWidget {
+class ShareDateWidget extends InheritedWidget {
+  ShareDateWidget({@required this.data, Widget child}) : super(child: child);
+
+  final int data; // 需要在子树中共享的数据，保存点击次数
+
+  // 定义一个便笺方法，方便子树中的widget获取共享数据
+  static ShareDateWidget of(BuildContext context) {
+    return context.inheritFromWidgetOfExactType(ShareDateWidget);
+  }
+
+  // 该回调决定当data发生变化时，是否通知子树中依赖data的Widget
   @override
-  WillPopScopeTestRouteState createState() => new WillPopScopeTestRouteState();
+  bool updateShouldNotify(ShareDateWidget old) {
+    // 如果返回true，则子树中依赖（build函数中有调用）本Widget
+    // 的子 widget 的state.didChangeDependencies会被调用
+    return old.data != data;
+  }
 }
 
-class WillPopScopeTestRouteState extends State<WillPopScopeTestRoute> {
-  DateTime _lastPressedAt;
+class _TestWidget extends StatefulWidget {
+  @override
+  __TestWidgetState createState() => new __TestWidgetState();
+}
 
+class __TestWidgetState extends State<_TestWidget> {
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        if (_lastPressedAt == null ||
-            DateTime.now().difference(_lastPressedAt) > Duration(seconds: 1)) {
-          _lastPressedAt = DateTime.now();
-          return false;
-        }
-        return true;
-      },
-      child: Container(
-        alignment: Alignment.center,
-        child: Text('1s内连续两次返回键退出'),
+    // 使用InheritedWidget中共享数据
+    return Text(ShareDateWidget.of(context).data.toString());
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // 父或祖先widget中的InheritedWidget 改变（updateShouldNotify返回true）时会被调用
+    // 如果build中没有依赖InheritedWidget,则此回调不会被调用。
+    print("Dependencies change");
+  }
+}
+
+class InheritedWidgetTestRoute extends StatefulWidget {
+  @override
+  _InheritedWidgetTestRouteState createState() =>
+      _InheritedWidgetTestRouteState();
+}
+
+class _InheritedWidgetTestRouteState extends State<InheritedWidgetTestRoute> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ShareDateWidget(
+        data: count,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: _TestWidget(), // 子widget中依赖ShareDataWidget
+            ),
+            RaisedButton(
+              child: Text('Increment'),
+              // 每点击一次，将count自增，然后重新build，ShareDataWidget的data将被更新
+              onPressed: () => setState(() => ++count),
+            )
+          ],
+        ),
       ),
     );
   }
