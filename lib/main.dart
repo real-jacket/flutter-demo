7f8065f9f1c861a1ba92b918c04638fdf3621f96
kete <1762982273@qq.com>
2019-04-09 18:23:17 +0800

    占位符、缓存图片

--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/io.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final title = 'Cached Images';
+
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primaryColor: Colors.blue,
-      ),
-      home: WebSocketRoute(),
-    );
-  }
-}
-
-// class FlexLayoutTestRoute extends StatelessWidget {
-//   FlexLayoutTestRoute({Key key, this.percent, this.controller})
-//       : super(key: key);
-
-//   final double percent;
-//   final AnimationController controller;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: new AppBar(
-//         title: new Text("Circular Percent Indicators"),
-//       ),
-//       body: Center(child: Container()),
-//     );
-//   }
-// }
-
-class WebSocketRoute extends StatefulWidget {
-  @override
-  _WebSocketRouteState createState() => _WebSocketRouteState();
-}
-
-class _WebSocketRouteState extends State<WebSocketRoute> {
-  TextEditingController _controller = TextEditingController();
-  IOWebSocketChannel channel;
-  String _text = "";
-
-  @override
-  void initState() {
-    channel = IOWebSocketChannel.connect('ws://echo.websocket.org');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: Text('websocket(内容回显)'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Form(
-              child: TextFormField(
-                controller: _controller,
-                decoration: InputDecoration(labelText: 'Send a message'),
-              ),
-            ),
-            StreamBuilder(
-              stream: channel.stream,
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  _text = '网络不通';
-                } else if (snapshot.hasData) {
-                  _text = "echo: " + snapshot.data;
-                }
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: Text(_text),
-                );
-              },
-            )
-          ],
+      title: title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(title),
+        ),
+        body: new Center(
+          child: new CachedNetworkImage(
+            placeholder: (context, url) => CircularProgressIndicator(),
+            imageUrl:
+                'http://b-ssl.duitang.com/uploads/item/201510/01/20151001212224_SJwzm.jpeg',
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _sendMessage,
-        tooltip: 'Send message',
-        child: Icon(Icons.send),
-      ),
     );
-  }
-
-  void _sendMessage() {
-    if (_controller.text.isNotEmpty) {
-      channel.sink.add(_controller.text);
-    }
-  }
-
-  @override
-  void dispose() {
-    channel.sink.close();
-    super.dispose();
   }
 }
