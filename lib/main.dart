5a5e6e66cd0cddee1847b51a1776c57e78c3bf7e
kete <1762982273@qq.com>
2019-04-10 10:51:07 +0800

    自定义button

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
-void main() {
-  runApp(new MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title = 'Cached Images';
+    final title = 'Gesture Demo';
 
     return new MaterialApp(
       title: title,
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text(title),
+      home: new MyHomePage(title: title),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(child: new MyButton()),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Our GestureDetector wraps our button
+    return new GestureDetector(
+      // When the child is tapped, show a snackbar
+      onTap: () {
+        final snackBar = new SnackBar(content: new Text("Tap"));
+
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+      // Our Custom Button!
+      child: new Container(
+        padding: new EdgeInsets.all(12.0),
+        decoration: new BoxDecoration(
+          color: Theme.of(context).buttonColor,
+          borderRadius: new BorderRadius.circular(8.0),
         ),
-        body: new Center(
-          child: new CachedNetworkImage(
-            placeholder: (context, url) => CircularProgressIndicator(),
-            imageUrl:
-                'http://b-ssl.duitang.com/uploads/item/201510/01/20151001212224_SJwzm.jpeg',
-          ),
-        ),
+        child: new Text('My Button'),
       ),
     );
   }
