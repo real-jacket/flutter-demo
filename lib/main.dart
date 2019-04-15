2fb43b8ae3d423df9676882b37f02cdd3f520cad
kete <1762982273@qq.com>
2019-04-15 16:45:15 +0800

    basic FILTER MENU Ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,312 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
-class Todo {
-  final String title;
-  final String description;
+void main() => runApp(MyApp());
 
-  Todo(this.title, this.description);
-}
-
-void main() {
-  runApp(new MaterialApp(
-    title: 'Passing Data',
-    home: new TodosScreen(
-      todos: new List.generate(
-        20,
-        (i) => new Todo(
-              'Todo $i',
-              'A description of what needs to be done for Todo $i',
-            ),
-      ),
-    ),
-  ));
-}
-
-class TodosScreen extends StatelessWidget {
-  final List<Todo> todos;
-
-  TodosScreen({Key key, @required this.todos}) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Todos'),
+    return MaterialApp(
+        title: 'UI Chalenge',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MainPage());
+  }
+}
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  double _imageHeight = 250;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _buildTimeline(),
+          _buildImage(),
+          _buildTopHeader(),
+          _buildProfileRow(),
+          _buildBottomPart(),
+        ],
       ),
-      body: new ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          return new ListTile(
-            title: new Text(todos[index].title),
-            // When a user taps on the ListTile, navigate to the DetailScreen.
-            // Notice that we're not only creating a new DetailScreen, we're
-            // also passing the current todo through to it!
-            onTap: () {
-              Navigator.push(
-                context,
-                new MaterialPageRoute(
-                  builder: (context) => new DetailScreen(todo: todos[index]),
+    );
+  }
+
+  Widget _buildImage() {
+    return ClipPath(
+      clipper: DialogonalClipper(),
+      child: Image.asset(
+        'images/birds.jpg',
+        fit: BoxFit.cover,
+        // height: _imageHeight,
+        colorBlendMode: BlendMode.srcOver,
+        color: Color.fromARGB(120, 20, 10, 40),
+      ),
+    );
+  }
+
+  Widget _buildTopHeader() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 40.0),
+      child: Row(
+        children: <Widget>[
+          Icon(Icons.menu, size: 32.0, color: Colors.white),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text(
+                "Timeline",
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300),
+              ),
+            ),
+          ),
+          Icon(Icons.linear_scale, color: Colors.white)
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileRow() {
+    return Padding(
+      padding: EdgeInsets.only(left: 16.0, top: _imageHeight / 2.5),
+      child: Row(
+        children: <Widget>[
+          CircleAvatar(
+            minRadius: 28.0,
+            maxRadius: 28.0,
+            backgroundImage: AssetImage('images/girl.jpg'),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  'Real Jacket',
+                  style: TextStyle(
+                      fontSize: 26.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400),
                 ),
-              );
-            },
-          );
-        },
+                Text(
+                  'Product designer',
+                  style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBottomPart() {
+    return Padding(
+      padding: EdgeInsets.only(top: _imageHeight),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _buildMyTaskHeader(),
+          _buildTaskslist(),
+        ],
+      ),
+    );
+  }
+
+  //Todo
+  Widget _buildTaskslist() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 0.0),
+        child: ListView(
+            children: tasks
+                .map((task) => TaskRow(
+                      task: task,
+                    ))
+                .toList()),
+      ),
+    );
+  }
+
+  Widget _buildMyTaskHeader() {
+    return Padding(
+      padding: EdgeInsets.only(left: 64.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'My Task',
+            style: TextStyle(fontSize: 34.0),
+          ),
+          Text(
+            'FEBRUARY 8, 2015',
+            style: TextStyle(color: Colors.grey, fontSize: 12.0),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTimeline() {
+    return Positioned(
+      top: 0.0,
+      bottom: 0.0,
+      left: 32.0,
+      child: Container(
+        width: 1.0,
+        color: Colors.grey[300],
       ),
     );
   }
 }
 
-class DetailScreen extends StatelessWidget {
-  // Declare a field that holds the Todo
-  final Todo todo;
-
-  // In the constructor, require a Todo
-  DetailScreen({Key key, @required this.todo}) : super(key: key);
+class DialogonalClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0.0, size.height - 60.0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
 
   @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
+
+class Task {
+  final String name;
+  final String category;
+  final String time;
+  final Color color;
+  final bool completed;
+
+  Task({this.name, this.category, this.time, this.color, this.completed});
+}
+
+List<Task> tasks = [
+  new Task(
+      name: "Catch up with Brian",
+      category: "Mobile Project",
+      time: "5pm",
+      color: Colors.orange,
+      completed: false),
+  new Task(
+      name: "Make new icons",
+      category: "Web App",
+      time: "3pm",
+      color: Colors.cyan,
+      completed: true),
+  new Task(
+      name: "Design explorations",
+      category: "Company Website",
+      time: "2pm",
+      color: Colors.pink,
+      completed: false),
+  new Task(
+      name: "Lunch with Mary",
+      category: "Grill House",
+      time: "12pm",
+      color: Colors.cyan,
+      completed: true),
+  new Task(
+      name: "Teem Meeting",
+      category: "Hangouts",
+      time: "10am",
+      color: Colors.cyan,
+      completed: true),
+  new Task(
+      name: "aaaa fsadf",
+      category: "Hangouts",
+      time: "10am",
+      color: Colors.cyan,
+      completed: true),
+  new Task(
+      name: "dasd Meefafting",
+      category: "Hangouts",
+      time: "10am",
+      color: Colors.cyan,
+      completed: true),
+  new Task(
+      name: "ddddd fafdaf",
+      category: "Hangouts",
+      time: "10am",
+      color: Colors.cyan,
+      completed: true),
+  new Task(
+      name: "ccccc bbnjm",
+      category: "Hangouts",
+      time: "10am",
+      color: Colors.cyan,
+      completed: true),
+];
+
+class TaskRow extends StatefulWidget {
+  final Task task;
+  final double dotSize = 12.0;
+
+  const TaskRow({Key key, this.task}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return TaskState();
+  }
+}
+
+class TaskState extends State<TaskRow> {
+  @override
   Widget build(BuildContext context) {
-    // Use the Todo to create our UI
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("${todo.title}"),
-      ),
-      body: new Padding(
-        padding: new EdgeInsets.all(16.0),
-        child: new Text('${todo.description}'),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding:
+                EdgeInsets.symmetric(horizontal: 32.0 - widget.dotSize / 2),
+            child: Container(
+              height: widget.dotSize,
+              width: widget.dotSize,
+              decoration: BoxDecoration(shape: BoxShape.circle,color: widget.task.color),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  widget.task.name,
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                Text(
+                  widget.task.category,
+                  style: TextStyle(fontSize: 12.0, color: Colors.grey),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Text(
+              widget.task.time,
+              style: TextStyle(fontSize: 12.0, color: Colors.grey),
+            ),
+          )
+        ],
       ),
     );
   }
