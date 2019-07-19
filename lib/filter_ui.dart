62b067e45608654bab32bf5ebe3fff97f139190e
kete <1762982273@qq.com>
2019-07-19 17:48:07 +0800

    add bottom_drawer

---
+++ lib/filter_ui.dart
@@ -0,0 +1,299 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class FilterUI extends StatefulWidget {
+  FilterUI({Key key}) : super(key: key);
+
+  @override
+  _FilterUIState createState() => _FilterUIState();
+}
+
+class _FilterUIState extends State<FilterUI> {
+  double _imageHeight = 250;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _buildTimeline(),
+          _buildImage(),
+          _buildTopHeader(),
+          _buildProfileRow(),
+          _buildBottomPart(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildImage() {
+    return ClipPath(
+      clipper: DialogonalClipper(),
+      child: Image.asset(
+        'images/birds.jpg',
+        fit: BoxFit.cover,
+        // height: _imageHeight,
+        colorBlendMode: BlendMode.srcOver,
+        color: Color.fromARGB(120, 20, 10, 40),
+      ),
+    );
+  }
+
+  Widget _buildTopHeader() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 40.0),
+      child: Row(
+        children: <Widget>[
+          Icon(Icons.menu, size: 32.0, color: Colors.white),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text(
+                "Timeline",
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300),
+              ),
+            ),
+          ),
+          Icon(Icons.linear_scale, color: Colors.white)
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileRow() {
+    return Padding(
+      padding: EdgeInsets.only(left: 16.0, top: _imageHeight / 2.5),
+      child: Row(
+        children: <Widget>[
+          CircleAvatar(
+            minRadius: 28.0,
+            maxRadius: 28.0,
+            backgroundImage: AssetImage('images/girl.jpg'),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  'Real Jacket',
+                  style: TextStyle(
+                      fontSize: 26.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400),
+                ),
+                Text(
+                  'Product designer',
+                  style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBottomPart() {
+    return Padding(
+      padding: EdgeInsets.only(top: _imageHeight),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _buildMyTaskHeader(),
+          _buildTaskslist(),
+        ],
+      ),
+    );
+  }
+
+  //Todo
+  Widget _buildTaskslist() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 0.0),
+        child: ListView(
+            children: tasks
+                .map((task) => TaskRow(
+                      task: task,
+                    ))
+                .toList()),
+      ),
+    );
+  }
+
+  Widget _buildMyTaskHeader() {
+    return Padding(
+      padding: EdgeInsets.only(left: 64.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'My Task',
+            style: TextStyle(fontSize: 34.0),
+          ),
+          Text(
+            'FEBRUARY 8, 2015',
+            style: TextStyle(color: Colors.grey, fontSize: 12.0),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTimeline() {
+    return Positioned(
+      top: 0.0,
+      bottom: 0.0,
+      left: 32.0,
+      child: Container(
+        width: 1.0,
+        color: Colors.grey[300],
+      ),
+    );
+  }
+}
+
+class DialogonalClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0.0, size.height - 60.0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
+
+class Task {
+  final String name;
+  final String category;
+  final String time;
+  final Color color;
+  final bool completed;
+
+  Task({this.name, this.category, this.time, this.color, this.completed});
+}
+
+List<Task> tasks = [
+  new Task(
+      name: "Catch up with Brian",
+      category: "Mobile Project",
+      time: "5pm",
+      color: Colors.orange,
+      completed: false),
+  new Task(
+      name: "Make new icons",
+      category: "Web App",
+      time: "3pm",
+      color: Colors.cyan,
+      completed: true),
+  new Task(
+      name: "Design explorations",
+      category: "Company Website",
+      time: "2pm",
+      color: Colors.pink,
+      completed: false),
+  new Task(
+      name: "Lunch with Mary",
+      category: "Grill House",
+      time: "12pm",
+      color: Colors.cyan,
+      completed: true),
+  new Task(
+      name: "Teem Meeting",
+      category: "Hangouts",
+      time: "10am",
+      color: Colors.cyan,
+      completed: true),
+  new Task(
+      name: "aaaa fsadf",
+      category: "Hangouts",
+      time: "10am",
+      color: Colors.cyan,
+      completed: true),
+  new Task(
+      name: "dasd Meefafting",
+      category: "Hangouts",
+      time: "10am",
+      color: Colors.cyan,
+      completed: true),
+  new Task(
+      name: "ddddd fafdaf",
+      category: "Hangouts",
+      time: "10am",
+      color: Colors.cyan,
+      completed: true),
+  new Task(
+      name: "ccccc bbnjm",
+      category: "Hangouts",
+      time: "10am",
+      color: Colors.cyan,
+      completed: true),
+];
+
+class TaskRow extends StatefulWidget {
+  final Task task;
+  final double dotSize = 12.0;
+
+  const TaskRow({Key key, this.task}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return TaskState();
+  }
+}
+
+class TaskState extends State<TaskRow> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding:
+                EdgeInsets.symmetric(horizontal: 32.0 - widget.dotSize / 2),
+            child: Container(
+              height: widget.dotSize,
+              width: widget.dotSize,
+              decoration: BoxDecoration(shape: BoxShape.circle,color: widget.task.color),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  widget.task.name,
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                Text(
+                  widget.task.category,
+                  style: TextStyle(fontSize: 12.0, color: Colors.grey),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Text(
+              widget.task.time,
+              style: TextStyle(fontSize: 12.0, color: Colors.grey),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
